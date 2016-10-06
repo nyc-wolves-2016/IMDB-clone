ef6b505a5fcdccb68758190a7e1ce1571cb59756
Neel Gupta <neelgupta2@gmail.com>
2016-10-06 11:33:20 -0400

    Add react components: TopMovie, TopMoviesView

---
+++ app/assets/javascripts/components/TopMovie.es6.jsx
@@ -0,0 +1,35 @@
+class TopMovie extends React.Component {
+  render() {
+    return(
+      <div class="movie-obj">
+        <ul>
+          <li>
+            <h2>1</h2>
+            <p>title 1</p>
+          </li>
+
+          <li>
+            <h2>2</h2>
+            <p>title 2</p>
+          </li>
+
+          <li>
+            <h2>3</h2>
+            <p>title 3</p>
+          </li>
+
+          <li>
+            <h2>4</h2>
+            <p>title 4</p>
+          </li>
+
+          <li>
+            <h2>5</h2>
+            <p>title 5</p>
+          </li>
+
+        </ul>
+      </div>
+    )
+  }
+}
